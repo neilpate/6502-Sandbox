68dd74b4bffe557f41c5486187d5049ce295848d
Neil <neil@premiergsolutions.com>
2021-10-28 21:22:25 +0200

    VIA Input working ok

--- Source/Assembly/led.65c02.asm
+++ Source/Assembly/led.65c02.asm
@@ -1,20 +1,30 @@
 	.target "6502"
 
+	VIA 		.equ $6000
+	VIA_REGB	.equ VIA + 0
+	VIA_REGA	.equ VIA + 1
+	VIA_DDRB 	.equ VIA + 2
+	VIA_DDRA 	.equ VIA + 3
 	
 
 	.org $8000
 	
 reset:	
 	lda #$ff	;Want to set DDRB to all output
-	sta $6002	;This is register 2 of the VIA
+	sta VIA_DDRB
 
-	lda #$55	;Some nice alternating bit pattern
-	sta $6000	;Output register B
+	lda #$00	;Want to set DDRA to all input
+	sta VIA_DDRA
+		
 
-	lda #$aa	;Some nice alternating bit pattern
-	sta $6000	;Output register B
+loop:
 
-	jmp reset
+	lda VIA_REGA	;Read Port A
+	sta $0			;Store it in RAM $00, just for debugging
+
+	sta VIA_REGB	;Output it to Port B
+
+	jmp loop
 
 	.org $fffc
 	.word reset
